e28101ef3a5930f48845450c596c13f83b01ff02
Jacob O'Brien <obria006@umn.edu>
2022-08-17 09:32:43 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Unet for tissue segmentation to be used with Autoinjector 2.0.',
+    author='Jacob OBrien',
+    license='',
+)
