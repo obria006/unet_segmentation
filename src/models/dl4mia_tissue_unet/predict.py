8c267f274ece4f35608f8bc8b6bca307ad346e85
Jacob O'Brien <obria006@umn.edu>
2022-08-26 13:54:29 +0200

    black autoformatting

--- src/models/dl4mia_tissue_unet/predict.py
+++ src/models/dl4mia_tissue_unet/predict.py
@@ -13,26 +13,27 @@ from src.models.dl4mia_tissue_unet.dl4mia_utils.img_utils import preprocess_imag
 from src.models.dl4mia_tissue_unet.dl4mia_utils.general import load_yaml
 from src.models.dl4mia_tissue_unet.model import UNet
 
-class Predicter():
+
+class Predicter:
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     @classmethod
-    def from_model(cls, model_path:str):
+    def from_model(cls, model_path: str):
         model = torch.load(model_path, map_location=cls.device)
         model.to(cls.device)
         return cls(model)
 
     @classmethod
-    def from_ckpt(cls, ckpt_path:str):
+    def from_ckpt(cls, ckpt_path: str):
         checkpoint = torch.load(ckpt_path, map_location=cls.device)
-        state_dict = checkpoint['model_state_dict']
-        model_dict = checkpoint['model_dict']
+        state_dict = checkpoint["model_state_dict"]
+        model_dict = checkpoint["model_dict"]
         print(f"Loaded checkpoint: {ckpt_path}")
         for key in checkpoint:
-            if 'state_dict' not in key and 'logger_data' not in key:
+            if "state_dict" not in key and "logger_data" not in key:
                 print(f"\t{key} = {checkpoint[key]}")
-        model = UNet(**model_dict['kwargs'])
+        model = UNet(**model_dict["kwargs"])
         model.load_state_dict(state_dict, strict=True)
         model.to(cls.device)
         return cls(model)
@@ -41,7 +42,7 @@ class Predicter():
         self.model = model
         self.model.eval()
 
-    def predict(self, image,gt=None):
+    def predict(self, image, gt=None):
         with torch.no_grad():
             im = preprocess_image(image)
             multiple_y = im.shape[1] // 8
@@ -55,7 +56,12 @@ class Predicter():
                 diff_x = 8 * (multiple_x + 1) - im.shape[2]
             else:
                 diff_x = 0
-            p2d = (diff_x // 2, diff_x - diff_x // 2, diff_y // 2, diff_y - diff_y // 2)  # last dim, second last dim
+            p2d = (
+                diff_x // 2,
+                diff_x - diff_x // 2,
+                diff_y // 2,
+                diff_y - diff_y // 2,
+            )  # last dim, second last dim
 
             im = F.pad(torch.from_numpy(im), p2d, "reflect").to(self.device)
 
@@ -66,38 +72,42 @@ class Predicter():
             pred_fg_thresholded = seed_map > 0.5
 
             if (diff_y - diff_y // 2) != 0:
-                pred_fg_thresholded = pred_fg_thresholded[diff_y // 2:-(diff_y - diff_y // 2), ...]
-                seed_map = seed_map[diff_y // 2:-(diff_y - diff_y // 2), ...]
+                pred_fg_thresholded = pred_fg_thresholded[
+                    diff_y // 2 : -(diff_y - diff_y // 2), ...
+                ]
+                seed_map = seed_map[diff_y // 2 : -(diff_y - diff_y // 2), ...]
             if (diff_x - diff_x // 2) != 0:
-                pred_fg_thresholded = pred_fg_thresholded[..., diff_x // 2:-(diff_x - diff_x // 2)]
-                seed_map = seed_map[..., diff_x // 2:-(diff_x - diff_x // 2)]
+                pred_fg_thresholded = pred_fg_thresholded[
+                    ..., diff_x // 2 : -(diff_x - diff_x // 2)
+                ]
+                seed_map = seed_map[..., diff_x // 2 : -(diff_x - diff_x // 2)]
 
         return pred_fg_thresholded, seed_map
 
-        
 
 def prepare_plot(origImage, origMask, actMask, predMask, title=None):
     # initialize our figure
     figure, ax = plt.subplots(nrows=2, ncols=2, figsize=(7, 7), dpi=100)
 
     # plot the original image, its mask, and the predicted mask
-    ax[0,0].imshow(origImage,cmap='gray')
-    ax[0,1].imshow(origMask)
-    pos = ax[1,0].imshow(actMask)
-    ax[1,1].imshow(predMask)
+    ax[0, 0].imshow(origImage, cmap="gray")
+    ax[0, 1].imshow(origMask)
+    pos = ax[1, 0].imshow(actMask)
+    ax[1, 1].imshow(predMask)
 
     # set the titles of the subplots
-    ax[0,0].set_title("Image")
-    ax[0,1].set_title("Original Mask")
-    ax[1,0].set_title("Activation Mask")
-    ax[1,1].set_title("Predicted Mask")
+    ax[0, 0].set_title("Image")
+    ax[0, 1].set_title("Original Mask")
+    ax[1, 0].set_title("Activation Mask")
+    ax[1, 1].set_title("Predicted Mask")
 
     # set the layout of the figure and display it
     figure.tight_layout()
-    figure.colorbar(pos, ax=ax[1,0])
+    figure.colorbar(pos, ax=ax[1, 0])
     if title is not None:
         figure.suptitle(title)
     figure.show()
+
 
 def make_predictions(predicter, imagePath, maskPath):
     # find the filename and generate the path to ground truth
@@ -112,19 +122,23 @@ def make_predictions(predicter, imagePath, maskPath):
     image = tifffile.imread(imagePath)
     orig = image.copy()
     t0 = time.time()
-    predMask, actMask = predicter.predict(image,gt = gtMask)
+    predMask, actMask = predicter.predict(image, gt=gtMask)
     print(f"Predict time: {time.time() - t0}")
-    predMask = (predMask*255).astype(np.uint8)
-    
-    # prepare a plot for visualization
-    prepare_plot(orig, gtMask, actMask, predMask,title=filename)
+    predMask = (predMask * 255).astype(np.uint8)
 
-def main(src_dir:str = "src/models/dl4mia_tissue_unet/results/20220824_144635", ckpt_name:str = 'best.pth'):
+    # prepare a plot for visualization
+    prepare_plot(orig, gtMask, actMask, predMask, title=filename)
+
+
+def main(
+    src_dir: str = "src/models/dl4mia_tissue_unet/results/20220824_163348",
+    ckpt_name: str = "best.pth",
+):
     print("[INFO] loading up test image paths...")
     test_dict_path = f"{src_dir}/test_dataset_dict.yaml"
     test_dataset_dict = load_yaml(test_dict_path)
-    data_dir = test_dataset_dict['kwargs']['data_dir']
-    data_type = test_dataset_dict['kwargs']['data_type']
+    data_dir = test_dataset_dict["kwargs"]["data_dir"]
+    data_type = test_dataset_dict["kwargs"]["data_type"]
     imagePaths = glob.glob(f"{data_dir}/{data_type}/images/*.tif")
     maskPaths = glob.glob(f"{data_dir}/{data_type}/masks/*.tif")
 
@@ -145,5 +159,5 @@ def main(src_dir:str = "src/models/dl4mia_tissue_unet/results/20220824_144635", 
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/models/dl4mia_tissue_unet/train.py
+++ src/models/dl4mia_tissue_unet/train.py
@@ -7,13 +7,24 @@ from src.models.dl4mia_tissue_unet.dataset import TwoDimensionalDataset
 from src.models.dl4mia_tissue_unet.model import UNet
 from src.models.dl4mia_tissue_unet.dl4mia_utils.general import print_dict, save_yaml
 from src.models.dl4mia_tissue_unet.dl4mia_utils.metrics import binary_sem_seg_metrics
-from src.models.dl4mia_tissue_unet.dl4mia_utils.train_utils import save_checkpoint, AverageMeter, Logger
+from src.models.dl4mia_tissue_unet.dl4mia_utils.train_utils import (
+    save_checkpoint,
+    AverageMeter,
+    Logger,
+)
+
 torch.backends.cudnn.benchmark = True
 
 
-class Trainer():
-
-    def __init__(self,train_dataset_dict, val_dataset_dict, test_dataset_dict, model_dict, config_dict):
+class Trainer:
+    def __init__(
+        self,
+        train_dataset_dict,
+        val_dataset_dict,
+        test_dataset_dict,
+        model_dict,
+        config_dict,
+    ):
         """
         Args
             train_dataset_dict: configuration dictionary for training dataset
@@ -27,35 +38,42 @@ class Trainer():
         self.test_dataset_dict = test_dataset_dict
         self.model_dict = model_dict
         self.config_dict = config_dict
-        self.device = torch.device("cuda:0" if self.config_dict['cuda'] else "cpu")
-        
-        print('loading datasets...')
-        self.train_dataset_it = self._create_data_loader(self.train_dataset_dict, shuffle=True, drop_last=True)
-        self.val_dataset_it = self._create_data_loader(self.val_dataset_dict, shuffle=True, drop_last=True)
+        self.device = torch.device("cuda:0" if self.config_dict["cuda"] else "cpu")
 
-        print('\ncreating model...')
+        print("loading datasets...")
+        self.train_dataset_it = self._create_data_loader(
+            self.train_dataset_dict, shuffle=True, drop_last=True
+        )
+        self.val_dataset_it = self._create_data_loader(
+            self.val_dataset_dict, shuffle=True, drop_last=True
+        )
+
+        print("\ncreating model...")
         self.model = self._create_model(model_dict)
         self.criterion = self._create_BCE_criterion()
         self.optimizer = self._set_optimizer(weight_decay=1e-4)
-        
 
     def _create_data_loader(self, dataset_dict, shuffle=True, drop_last=True):
-        dataset = TwoDimensionalDataset(**dataset_dict['kwargs'])
-        data_loader = torch.utils.data.DataLoader(dataset, batch_size=dataset_dict['batch_size'],
-                                                    shuffle=shuffle, drop_last=drop_last,
-                                                    num_workers=dataset_dict['workers'],
-                                                    pin_memory=True if self.config_dict['cuda'] else False)
+        dataset = TwoDimensionalDataset(**dataset_dict["kwargs"])
+        data_loader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=dataset_dict["batch_size"],
+            shuffle=shuffle,
+            drop_last=drop_last,
+            num_workers=dataset_dict["workers"],
+            pin_memory=True if self.config_dict["cuda"] else False,
+        )
         return data_loader
 
     def _create_model(self, model_dict):
         # set model
-        model = UNet(**model_dict['kwargs'])
+        model = UNet(**model_dict["kwargs"])
         model = torch.nn.DataParallel(model)
         model.to(self.device)
         return model
 
-    def _create_CE_criterion(self, weight:list=[1.0, 1.0, 5.0]):
-        if self.config_dict['cuda']:
+    def _create_CE_criterion(self, weight: list = [1.0, 1.0, 5.0]):
+        if self.config_dict["cuda"]:
             criterion = nn.CrossEntropyLoss(weight=torch.tensor(weight).cuda())
         else:
             criterion = nn.CrossEntropyLoss(weight=torch.tensor(weight))
@@ -69,85 +87,123 @@ class Trainer():
         criterion.to(self.device)
         return criterion
 
-    def _set_optimizer(self, weight_decay:float=1e-4):
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config_dict['train_lr'], weight_decay=weight_decay)
+    def _set_optimizer(self, weight_decay: float = 1e-4):
+        optimizer = torch.optim.Adam(
+            self.model.parameters(),
+            lr=self.config_dict["train_lr"],
+            weight_decay=weight_decay,
+        )
         return optimizer
 
     def begin_training(self):
-        
-        if self.config_dict['save']:
-            if not os.path.exists(self.config_dict['save_dir']):
-                os.makedirs(self.config_dict['save_dir'])
-            save_yaml(dict_=self.train_dataset_dict, filepath=f"{self.config_dict['save_dir']}/train_dataset_dict.yaml")
-            save_yaml(dict_=self.val_dataset_dict, filepath=f"{self.config_dict['save_dir']}/val_dataset_dict.yaml")
-            save_yaml(dict_=self.test_dataset_dict, filepath=f"{self.config_dict['save_dir']}/test_dataset_dict.yaml")
-            save_yaml(dict_=self.model_dict, filepath=f"{self.config_dict['save_dir']}/model_dict.yaml")
-            save_yaml(dict_=self.config_dict, filepath=f"{self.config_dict['save_dir']}/config_dict.yaml")
 
+        if self.config_dict["save"]:
+            if not os.path.exists(self.config_dict["save_dir"]):
+                os.makedirs(self.config_dict["save_dir"])
+            save_yaml(
+                dict_=self.train_dataset_dict,
+                filepath=f"{self.config_dict['save_dir']}/train_dataset_dict.yaml",
+            )
+            save_yaml(
+                dict_=self.val_dataset_dict,
+                filepath=f"{self.config_dict['save_dir']}/val_dataset_dict.yaml",
+            )
+            save_yaml(
+                dict_=self.test_dataset_dict,
+                filepath=f"{self.config_dict['save_dir']}/test_dataset_dict.yaml",
+            )
+            save_yaml(
+                dict_=self.model_dict,
+                filepath=f"{self.config_dict['save_dir']}/model_dict.yaml",
+            )
+            save_yaml(
+                dict_=self.config_dict,
+                filepath=f"{self.config_dict['save_dir']}/config_dict.yaml",
+            )
 
         def lambda_(epoch):
             return pow((1 - ((epoch) / 200)), 0.9)
 
-        scheduler = torch.optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda=lambda_, )
+        scheduler = torch.optim.lr_scheduler.LambdaLR(
+            self.optimizer,
+            lr_lambda=lambda_,
+        )
 
         # Logger
-        logger = Logger(('train', 'val', 'ap'), 'loss')
+        logger = Logger(("train", "val", "ap"), "loss")
 
         # resume
         start_epoch = 0
         best_ap = 0
-        if self.config_dict['resume_path'] is not None and os.path.exists(self.config_dict['resume_path']):
-            print('Resuming model from {}'.format(self.config_dict['resume_path']), flush=True)
-            state = torch.load(self.config_dict['resume_path'])
-            start_epoch = state['epoch'] + 1
-            best_ap = state['best_ap']
-            self.model.load_state_dict(state['model_state_dict'], strict=True)
-            self.optimizer.load_state_dict(state['optim_state_dict'])
-            logger.data = state['logger_data']
+        if self.config_dict["resume_path"] is not None and os.path.exists(
+            self.config_dict["resume_path"]
+        ):
+            print(
+                "Resuming model from {}".format(self.config_dict["resume_path"]),
+                flush=True,
+            )
+            state = torch.load(self.config_dict["resume_path"])
+            start_epoch = state["epoch"] + 1
+            best_ap = state["best_ap"]
+            self.model.load_state_dict(state["model_state_dict"], strict=True)
+            self.optimizer.load_state_dict(state["optim_state_dict"])
+            logger.data = state["logger_data"]
 
-        for epoch in range(start_epoch, start_epoch + self.config_dict['n_epochs']):
-            print('Starting epoch {}'.format(epoch), flush=True)
+        for epoch in range(start_epoch, start_epoch + self.config_dict["n_epochs"]):
+            print("Starting epoch {}".format(epoch), flush=True)
             train_loss = self.train()
             val_loss, val_ap = self.val()
 
             scheduler.step()
-            print('===> train loss: {:.2f}'.format(train_loss), flush=True)
-            print('===> val loss: {:.2f}, val ap: {:.2f}'.format(val_loss, val_ap), flush=True)
+            print("===> train loss: {:.2f}".format(train_loss), flush=True)
+            print(
+                "===> val loss: {:.2f}, val ap: {:.2f}".format(val_loss, val_ap),
+                flush=True,
+            )
 
-            logger.add('train', train_loss)
-            logger.add('val', val_loss)
-            logger.add('ap', val_ap)
-            logger.plot(save=self.config_dict['save'], save_dir=self.config_dict['save_dir'])
+            logger.add("train", train_loss)
+            logger.add("val", val_loss)
+            logger.add("ap", val_ap)
+            logger.plot(
+                save=self.config_dict["save"], save_dir=self.config_dict["save_dir"]
+            )
 
             is_best = val_ap > best_ap
             best_ap = max(val_ap, best_ap)
 
-            if self.config_dict['save']:
+            if self.config_dict["save"]:
                 trainable_state = {
-                    'epoch': epoch,
-                    'val_loss': val_loss,
-                    'val_ap': val_ap,
-                    'best_ap': best_ap,
-                    'train_cuda': self.config_dict['cuda'],
-                    'model_dict': self.model_dict,
-                    'model_state_dict': self.model.module.state_dict(), # remove .module if not using nn.DataParallel model
-                    'optim_state_dict': self.optimizer.state_dict(),
-                    'logger_data': logger.data,
+                    "epoch": epoch,
+                    "val_loss": val_loss,
+                    "val_ap": val_ap,
+                    "best_ap": best_ap,
+                    "train_cuda": self.config_dict["cuda"],
+                    "model_dict": self.model_dict,
+                    "model_state_dict": self.model.module.state_dict(),  # remove .module if not using nn.DataParallel model
+                    "optim_state_dict": self.optimizer.state_dict(),
+                    "logger_data": logger.data,
                 }
-                save_checkpoint(trainable_state, is_best, save_dir=self.config_dict['save_dir'], name='last.pth')
+                save_checkpoint(
+                    trainable_state,
+                    is_best,
+                    save_dir=self.config_dict["save_dir"],
+                    name="last.pth",
+                )
 
     def train(self):
         loss_meter = AverageMeter()
         self.model.train()
         for param_group in self.optimizer.param_groups:
-            print('learning rate: {}'.format(param_group['lr']), flush=True)
+            print("learning rate: {}".format(param_group["lr"]), flush=True)
         for i, sample in enumerate(tqdm(self.train_dataset_it)):
-            images = sample['image']  # B 1 Z Y X
-            semantic_masks = sample['semantic_mask']  # B 1 Z Y X
+            images = sample["image"]  # B 1 Z Y X
+            semantic_masks = sample["semantic_mask"]  # B 1 Z Y X
             # semantic_masks.squeeze_(1)  #FIXME B Z Y X (loss expects this format) JO But it doesn't match output
             output = self.model(images)  # B 3 Z Y X
             # loss = self.criterion(output, semantic_masks.long())  # B 1 Z Y X
-            loss = self.criterion(output, semantic_masks.float())  # B 1 Z Y X JO only for  BCEWithLogitsLoss
+            loss = self.criterion(
+                output, semantic_masks.float()
+            )  # B 1 Z Y X JO only for  BCEWithLogitsLoss
             loss = loss.mean()
             self.optimizer.zero_grad()
             loss.backward()
@@ -163,12 +219,14 @@ class Trainer():
         with torch.no_grad():
             for i, sample in enumerate(tqdm(self.val_dataset_it)):
 
-                images = sample['image']  # B 1 Z Y X
-                semantic_masks = sample['semantic_mask']  # B 1 Z Y X
+                images = sample["image"]  # B 1 Z Y X
+                semantic_masks = sample["semantic_mask"]  # B 1 Z Y X
                 # semantic_masks.squeeze_(1)  #FIXME B Z Y X (loss expects this format) JO But it doesn't match output
                 output = self.model(images)  # B 3 Z Y X
                 # loss = self.criterion(output, semantic_masks.long())  # B 1 Z Y X
-                loss = self.criterion(output, semantic_masks.float())  # B 1 Z Y X JO only for  BCEWithLogitsLoss
+                loss = self.criterion(
+                    output, semantic_masks.float()
+                )  # B 1 Z Y X JO only for  BCEWithLogitsLoss
                 loss = loss.mean()
                 loss_meter.update(loss.item())
                 # bin_metric = BinaryMetrics(activation='0-1') # Does a sigmoid and threshold activation
@@ -178,14 +236,18 @@ class Trainer():
                     output_softmax = torch.sigmoid(output[b])
                     prediction_fg = output_softmax[0, ...].cpu().detach().numpy()
                     pred_fg_thresholded = (prediction_fg > 0.5).astype(int)
-                    acc, dice, prec, spec, rec = binary_sem_seg_metrics(y_true=semantic_masks[b, 0, ...].cpu().detach().numpy(), y_pred=pred_fg_thresholded)
+                    acc, dice, prec, spec, rec = binary_sem_seg_metrics(
+                        y_true=semantic_masks[b, 0, ...].cpu().detach().numpy(),
+                        y_pred=pred_fg_thresholded,
+                    )
                     # sc = matching_dataset(y_pred=[pred_fg_thresholded], y_true=[semantic_masks[b, 0, ...].cpu().detach().numpy()])
                     # instance_map, _ = ndimage.label(pred_fg_thresholded)
-                #     # sc = matching_dataset(y_pred=[instance_map], y_true=[instance[b, 0, ...].cpu().detach().numpy()],
-                #     #                     thresh=0.5, show_progress=False)
+                    #     # sc = matching_dataset(y_pred=[instance_map], y_true=[instance[b, 0, ...].cpu().detach().numpy()],
+                    #     #                     thresh=0.5, show_progress=False)
                     average_precision_meter.update(dice)
 
         return loss_meter.avg, average_precision_meter.avg
+
 
 def main(
     data_dir: str = "data/processed/uncropped",
@@ -195,8 +257,8 @@ def main(
     n_levels: int = 3,
     in_size: tuple = (128, 128),
     init_lr: float = 5e-4,
-    n_epochs: int = 4,
-    batch_size: int = 16,
+    n_epochs: int = 15,
+    batch_size: int = 8,
     save: bool = True,
 ):
     cfg = Config(
@@ -209,10 +271,10 @@ def main(
         init_lr=init_lr,
         n_epochs=n_epochs,
         batch_size=batch_size,
-        save=save
-        )
+        save=save,
+    )
     train_dataset_dict = cfg.TRAIN_DATASET_DICT
-    print('Training dictionary:')
+    print("Training dictionary:")
     print_dict(train_dataset_dict)
     val_dataset_dict = cfg.VAL_DATASET_DICT
     print("\nValidation dictionary:")
@@ -227,8 +289,11 @@ def main(
     print("\nConfig dictionary:")
     print_dict(config_dict)
     print()
-    model_trainer = Trainer(train_dataset_dict, val_dataset_dict, test_dataset_dict, model_dict, config_dict)
+    model_trainer = Trainer(
+        train_dataset_dict, val_dataset_dict, test_dataset_dict, model_dict, config_dict
+    )
     model_trainer.begin_training()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
