cb9145acdf73d831387a36d4b125770751468e13
Jacob O'Brien <obria006@umn.edu>
2023-05-22 10:51:44 -0500

    Remove padding = "same"

--- src/models/dl4mia_tissue_unet/model_v2.py
+++ src/models/dl4mia_tissue_unet/model_v2.py
@@ -10,10 +10,9 @@ import torch
 import torch.nn as nn
 
 
-def ConvBlock(in_channels, out_channels, kernel_size=3, stride=1, padding="same"):
+def ConvBlock(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
     return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=1, bias=False),
-        # nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding="same", bias=False),
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=padding, bias=False),
         nn.BatchNorm2d(out_channels),
         nn.ReLU(inplace=True),
     )
@@ -24,7 +23,6 @@ def FinalBlock(in_channels, out_channels):
         # ConvBlock(in_channels, in_channels,  kernel_size = 1),
         # ConvBlock(in_channels, in_channels,  kernel_size = 1),
         nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
-        # nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding="same")
     )
 
 
